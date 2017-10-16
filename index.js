e391ffe877f2f9ce2dbc532efcfa35b832fd7e37
GitPis <matthieu.pisquet@gmail.com>
2017-10-16 19:20:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,4 @@
 module.exports = function(sails) {
-
-//    var install = require('./lib/install.js');
-//    var uninstall = require('./lib/uninstall.js');
-
-    gladys.on('ready', function() {
-    });
-
-    // Le 'return' permet de retourner toutes les fonctions que Gladys peut utiliser en dehors du module
     return {
- //       install: install,
- //       uninstall: uninstall
     };
 };
